5af3e00c688d9bc24a5e8a0bb550bd9bd5143977
ayoussefinia <alex.youssefinia@gmail.com>
2020-11-04 23:13:32 -0600

    resolved bug

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
-
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
+
+
+app.use(cors());
+
+mongoose.connect('mongodb://yoshi:test123@ds363996.mlab.com:63996/gql-ninja');
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
